4c87b6e5adee272dcdf749d9dd8ddea73f37fc4a
omrisk <omrisk@gmail.com>
2017-04-06 10:38:02 +0300

    Added MuiThemes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,21 +1,30 @@
 import React, {Component} from 'react';
 import logo from '../../logo.svg';
 import './App.css';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
     render() {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Hello World!</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/Components/App/App.js</code> and save to reload.<br/>
-                    The file watcher is still in beta so it may not work perfectly
+        let style = {
+            margin: 12,
+        };
 
-                </p>
-            </div>
+        return (
+            <MuiThemeProvider>
+                <div className="App">
+                    <RaisedButton label="Default" style={style}/>
+                    <div className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h2>Hello World!</h2>
+                    </div>
+                    <p className="App-intro">
+                        To get started, edit <code>src/Components/App/App.js</code> and save to reload.<br/>
+                        The file watcher is still in beta so it may not work perfectly
+
+                    </p>
+                </div>
+            </MuiThemeProvider>
         );
     }
 }
